17d7bab840172ff69c712f78e84a8346f972ece6
SIDDHARTH JAIN <103947670+siddhaarthh@users.noreply.github.com>
2023-05-07 02:37:36 +0530

    checking

---
+++ check.java
@@ -0,0 +1,10 @@
+
+public class check {
+    public static void main(String[] args){
+        System.out.println("enter number");
+        
+        char l = '\u0041';
+        System.out.println(l);
+
+    }
+}
