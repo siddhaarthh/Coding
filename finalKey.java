02dc8f93fadefdd43aaf3bb5ed9ce0f69ba09e46
SIDDHARTH JAIN <103947670+siddhaarthh@users.noreply.github.com>
2023-05-10 14:15:59 +0530

    final key implementation

---
+++ finalKey.java
@@ -0,0 +1,15 @@
+class person{
+    final int age=18;
+    void agee()
+    {
+        age=19;
+    }
+}
+
+public class finalKey {
+    public static void main(String[] args)
+    {
+        person p=new person();
+        p.agee();
+    }
+}
