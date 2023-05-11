95a56543103d9c6e16d93ffc991fa7a454406621
SIDDHARTH JAIN <103947670+siddhaarthh@users.noreply.github.com>
2023-05-11 15:33:42 +0530

    int to char

---
+++ int-char.java
@@ -0,0 +1,10 @@
+public class int-char {
+    public static void main(String args[]){
+        int num1=2;
+        int num2=3;
+        char a=(char)num1;
+        char b=(char)num2;
+        System.out.println(a    b);
+    }
+    
+}
