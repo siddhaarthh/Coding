686f28c3574bfd38c1ab18f081aea6c8d5cf5d39
SIDDHARTH JAIN <103947670+siddhaarthh@users.noreply.github.com>
2023-05-11 15:33:13 +0530

    integer to char

---
+++ int_char.java
@@ -0,0 +1,12 @@
+public class int_char {
+    public static void main(String args[]){
+        int num1 = 80;
+        int num2 = 81;
+        char a = (char)num1;
+        char b = (char)num2;
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
+    
+}
