85ef7d13be9219266c9baf246a61e605f29b6e2e
SIDDHARTH JAIN <103947670+siddhaarthh@users.noreply.github.com>
2023-05-15 15:09:55 +0530

    largest number

---
+++ max_3.java
@@ -0,0 +1,12 @@
+public class max_3 {
+    public static void main(String[] args)
+    {
+        int a=3;
+        int b=6;
+        int c=1;
+        int p,q;
+        p=Math.max(a,b);
+        q=Math.max(p,c);
+        System.out.println(q + " is largest number");
+    }
+}
