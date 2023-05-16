67208725e5707ef78405b5c51ec19a2db33539a9
SIDDHARTH JAIN <103947670+siddhaarthh@users.noreply.github.com>
2023-05-16 16:35:53 +0530

    num from sum of two prime number

---
+++ num_by_sumoftwo_prime.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class num_by_sumoftwo_prime {
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        System.out.Scanner("enter num");
+        int a=s.nextInt();
+        int i,j=0;
+        for(i=2;i<=a/2;i++)
+        {
+           if(a/i==0)
+        {
+                j++;
+        }
+        }
+        if(j>0)
+        {
+
+        }
+    }    
+}
