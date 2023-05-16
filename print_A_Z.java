888a86e6b6b708d6403795d48dbfcd9efcc097cd
SIDDHARTH JAIN <103947670+siddhaarthh@users.noreply.github.com>
2023-05-17 02:13:49 +0530

    print capital alphabets

---
+++ print_A_Z.java
@@ -0,0 +1,11 @@
+
+public class print_A_Z {
+    public static void main(String args[]){
+        
+        char a;
+        for(a='A';a<='Z';a++)
+        {
+            System.out.println(a);
+        }
+    }
+}
