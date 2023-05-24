22628e3c8faa187e9f349bbd470569672019c553
SIDDHARTH JAIN <103947670+siddhaarthh@users.noreply.github.com>
2023-05-24 11:02:49 +0530

    tree map

---
+++ TreeMap.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+ class TreeMap {
+    public static void main(String[] args){
+        TreeMap<String, Integer> EvenNum = new TreeMap<>();
+        EvenNum.put("One", 1);
+        System.out.println(EvenNum);
+    }
+}
